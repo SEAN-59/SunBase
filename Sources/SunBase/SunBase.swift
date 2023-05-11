03dceb7798460565a211660599b0e5b761bd66fb
Company <ksg3452@gmail.com>
2023-05-11 14:03:11 +0900

    TestCommit

--- Sources/SunBase/SunBase.swift
+++ Sources/SunBase/SunBase.swift
@@ -1,6 +1,34 @@
-public struct SunBase {
-    public private(set) var text = "Hello, World!"
+#if os(iOS) || os(tvOS)
+    import UIKit
+#else
+    import AppKit
+#endif
 
-    public init() {
+typealias originX = CGPoint
+
+//public struct SunBase {
+//    private var isToastOn: Bool = false
+//
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {}
+//
+//
+//}
+
+public final class SunBase {
+    public private(set) var text = "Hello, World!"
+}
+
+
+class SunLabel: UILabel {
+    private var padding: UIEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 0)
+    
+    public func padding(top: CGFloat,left: CGFloat,bottom: CGFloat,right: CGFloat) {
+        padding.top = top
+        padding.left = left
+        padding.bottom = bottom
+        padding.right = right
     }
+//    UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 }
