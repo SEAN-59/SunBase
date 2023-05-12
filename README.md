7ca4f3366794eb052e3f0fd5da8ba6e6889aeb72
Company <ksg3452@gmail.com>
2023-05-12 18:00:11 +0900

    05.12 Toast Message & Timer

--- README.md
+++ README.md
@@ -3,9 +3,58 @@
 A description of this package.
 
 ## 구현 목록
-[] Toast Messagae
-[] Radio button
-[] Check Box
-[] Drop down
-[] Custom Alert
+[x] Toast Messagae <br>
+- 가장 기본이 되는 토스트 메세지 구현 완료
+```swift
+    showToast(message: String, type: simpleType, constatns: CGFloat, duration: CGFloat, delay:CGFloat ,isRepeat: Bool)
+```
+- message만 입력해도 바로 토스트 메세지는 호출되나 조금더 세부 사항을 설정하기 위해서는 다른 입력 파라미터들도 조정을 해준다.
 
+```swift
+    showIndicator()
+    stopIndicator()
+```
+
+- 따로 설정할 것 없이 showIndicator 와 stopIndicator로만 동작을 제어한다.
+
+[ ] Radio button <br>
+
+[ ] Check Box <br>
+
+[ ] Drop down <br>
+
+[ ] Custom Alert <br>
+
+[x] Timer <br>
+- 타이머 실행과 타이머 중간에 멈추게 하는 동작이 되는 기능 구현
+- 싱글턴으로 만들어져 있어서 다음과 같은 방법으로 접근하는걸 추천
+
+```swift
+    let sunTimer = SunTimer.shared
+```
+
+```swift
+    startTimer(interval: Double, repeats: Bool, completion: @escaping (_ isStop: Bool) -> ())
+    
+    stopTimer(completion: (Bool) -> ())
+```
+- 2가지의 함수로만 동작을 제어함
+
+- startTimer의 경우에는 타이머 시간과 반복을 입력해주면 클로저로 들어오는 값을 가지고 원하는 작업을 진행하면 됨
+- 이때 들어오는 값은 Bool로 타이머가 제시간에 맞게 종료 되면 true를 받을 수 있음
+```swift
+    sunTimer.startTimer(interval: 3) { isStop in
+            print(isStop)
+    }
+    
+    // true
+```
+
+- stopTimer의 경우에는 타이머가 동작 중인 경우에 해당 타이머를 중간에 끊어주기 위해 사용하는 함수
+```swift
+    sunTimer.stopTimer { isStop in
+        print(isStop)
+    }
+    
+    // true
+```
--- Sources/SunBase/SunBase.swift
+++ Sources/SunBase/SunBase.swift
@@ -1,85 +1,223 @@
 #if os(iOS) || os(tvOS)
-    import UIKit
+import UIKit
 #else
-    import AppKit
+import AppKit
 #endif
 
-typealias originX = CGPoint
-
-//public struct SunBase {
-//    private var isToastOn: Bool = false
-//
-//    public private(set) var text = "Hello, World!"
-//ㄴㅇㄴㅇㄴㅇㄴㅇㄴ
-//    public init() {}
-//
-//
-//}
 
 public final class SunBase {
-    public private(set) var text = "Hello, World!"
+    //    public private(set) var text = "Hello, World!"
 }
 
-public extension UIView {
+//MARK: - Timer
+public final class SunTimer {
+    public static let shared = SunTimer()
+    private var timer = Timer()
     
-    enum toastType{
-        case simple
-        case top
-        case down
-        case right
-        case left
+    private init() {
+        
     }
     
-    func showToast(message: String, type: toastType = .simple) {
-        var a = 0
-        let toast: SunLabel = makeToast(message: message)
-        self.addToast(toast)
-        switch type{
-        case .simple:
-            self.simpleToast(toast: toast)
-            a += 1
-            print(a)
-        case .top:
-            print("up")
-        case .down:
-            print("up")
-        case .right:
-            print("up")
-        case .left:
-            print("up")
+    public func startTimer(interval: Double, repeats: Bool = false, completion: @escaping (_ isStop: Bool) -> ()) {
+        let nowDate = Date()
+        
+        // 타이머 시작전 다른 타이머가 동작 중이면 중지
+        if self.timer.isValid {
+            self.timer.invalidate()
+        }
+        
+        self.timer = .scheduledTimer(withTimeInterval: interval,
+                                     repeats: repeats,
+                                     block: { [weak self] timer in
+            guard let weakSelf = self else {return}
+            let firstTime = Date().timeIntervalSince(nowDate)
+            let secondTime = Int(interval - firstTime)
+            
+            if secondTime == 0 {
+                // 정지는 되었겠지만 그래도 혹시 몰라서
+                weakSelf.timer.invalidate()
+                print("TIMER_END")
+                completion(true)
+            } else {
+                completion(false)
+            }
+        })
+    }
+    
+    public func stopTimer(completion: (Bool) -> ()) {
+        // 중간에 타이머를 정지시키기 위해서 사용
+        if self.timer.isValid {
+            self.timer.invalidate()
+            completion(true)
+        } else {
+            completion(false)
+        }
+    }
+    
+}
+
+//MARK: - TOAST MESSAGE
+public extension UIView {
+    enum simpleType{
+        case top
+        case down
+    }
+    
+    func showToast(message: String, type: simpleType = .down, constatns: CGFloat = 100, duration: CGFloat = 2.0, delay:CGFloat = 0.4 ,isRepeat: Bool = false) {
+        var isSunLabel: Bool = false
+        
+        if isRepeat {
+            self.subviews.forEach {
+                if $0 is SunLabel {
+                    isSunLabel = true
+                }
+            }
+        }
+        
+        if !isSunLabel {
+            let toast: SunLabel = makeToast(message: message)
+            self.addToast(toast)
+            
+            switch type{
+            case .down:
+                self.simpleDownToast(toast: toast,constant: constatns,duration: duration,delay: delay)
+            case .top:
+                self.simpleTopToast(toast: toast,constant: constatns,duration: duration,delay: delay)
+            }
+//            self.willRemoveSubview(toast)
+        }
+    }
+    
+    func showIndicator() {
+        let indicator: SunIndicator = makeIndicatorToast()
+        self.isUserInteractionEnabled = false
+        self.subviews.forEach {
+            if $0 is SunIndicator {
+                $0.removeFromSuperview()
+            }
+        }
+        self.addToast(indicator)
+        indicator.startAnimating()
+
+        self.indicatorToast(toast: indicator)
+    }
+    
+    func stopIndicator() {
+        self.isUserInteractionEnabled = true
+        self.subviews.forEach {
+            if $0 is SunIndicator {
+                $0.removeFromSuperview()
+            }
         }
         
     }
     
-    private func simpleToast(toast: SunLabel) {
+    //MARK: - Simple Toast Animation
+    private func simpleDownToast(toast: SunLabel, constant: CGFloat, duration:CGFloat, delay:CGFloat) {
         toast.translatesAutoresizingMaskIntoConstraints = false
         [
             NSLayoutConstraint.init(item: toast,
-                                attribute: .centerX,
-                                relatedBy: .equal,
+                                    attribute: .centerX,
+                                    relatedBy: .equal,
                                     toItem: toast.superview,
-                                attribute: .centerX,
-                                multiplier: 1,
-                                 constant: 0),
+                                    attribute: .centerX,
+                                    multiplier: 1,
+                                    constant: 0),
+            
             NSLayoutConstraint.init(item: toast,
-                                attribute: .bottom,
-                                relatedBy: .equal,
-                                toItem: toast.superview,
-                                attribute: .bottom,
-                                multiplier: 1,
-                                 constant: -200)
+                                    attribute: .bottom,
+                                    relatedBy: .equal,
+                                    toItem: toast.superview,
+                                    attribute: .bottom,
+                                    multiplier: 1,
+                                    constant: constant * -1),
+            
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .leading,
+                                    relatedBy: .greaterThanOrEqual,
+                                    toItem: toast.superview,
+                                    attribute: .leading,
+                                    multiplier: 1,
+                                    constant: 30)
         ].forEach{$0.isActive = true}
         
-        UIView.animate(withDuration: 3.0,
-                       delay: 0.1,
+        UIView.animate(withDuration: duration,
+                       delay: delay,
                        options: .curveEaseOut,
                        animations: {
             toast.alpha = 0.0
         },
                        completion: { _ in
-            print("good")
+            toast.removeFromSuperview()
+            
         })
-
+        
+    }
+    
+    private func simpleTopToast(toast: SunLabel, constant: CGFloat, duration:CGFloat, delay:CGFloat) {
+        toast.translatesAutoresizingMaskIntoConstraints = false
+        [
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .centerX,
+                                    relatedBy: .equal,
+                                    toItem: toast.superview,
+                                    attribute: .centerX,
+                                    multiplier: 1,
+                                    constant: 0),
+            
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .top,
+                                    relatedBy: .equal,
+                                    toItem: toast.superview,
+                                    attribute: .top,
+                                    multiplier: 1,
+                                    constant: constant),
+            
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .leading,
+                                    relatedBy: .greaterThanOrEqual,
+                                    toItem: toast.superview,
+                                    attribute: .leading,
+                                    multiplier: 1,
+                                    constant: 30)
+        ].forEach{$0.isActive = true}
+        
+        UIView.animate(withDuration: duration,
+                       delay: delay,
+                       options: .curveEaseOut,
+                       animations: {
+            toast.alpha = 0.0
+        },
+                       completion: { _ in
+            toast.removeFromSuperview()
+        })
+        
+    }
+    
+    
+    private func indicatorToast(toast: SunIndicator) {
+        toast.translatesAutoresizingMaskIntoConstraints = false
+        [
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .centerX,
+                                    relatedBy: .equal,
+                                    toItem: toast.superview,
+                                    attribute: .centerX,
+                                    multiplier: 1,
+                                    constant: 0),
+            
+            NSLayoutConstraint.init(item: toast,
+                                    attribute: .centerY,
+                                    relatedBy: .equal,
+                                    toItem: toast.superview,
+                                    attribute: .centerY,
+                                    multiplier: 1,
+                                    constant: 0),
+            
+            toast.widthAnchor.constraint(equalToConstant: 100),
+            toast.heightAnchor.constraint(equalToConstant: 100)
+        ].forEach{$0.isActive = true}
+        
     }
     
     private func addToast<T>(_ toast: T) {
@@ -87,31 +225,48 @@ public extension UIView {
         self.addSubview(toast)
     }
     
-    private func makeToast(message: String) -> SunLabel {
-        let label: SunLabel = {
-            let label = SunLabel()
-            
-            label.backgroundColor = .black.withAlphaComponent(0.5)
-            label.textColor = .white
-            label.font = UIFont.systemFont(ofSize: 20.0)
-            label.numberOfLines = 1
-            label.adjustsFontSizeToFitWidth = true
-            label.minimumScaleFactor = 0.6
-            label.textAlignment = .center
-            label.alpha = 1.0
-            label.text = message
-            label.layer.cornerRadius = label.font.pointSize / 2
-            label.clipsToBounds = true
-            return label
+    private func makeIndicatorToast() -> SunIndicator {
+        let indicator: SunIndicator = {
+            let indicator = SunIndicator()
+            indicator.backgroundColor = .black.withAlphaComponent(0.5)
+            indicator.style = .large
+            indicator.color = .white
+            indicator.alpha = 1.0
+            indicator.layer.cornerRadius = 15
+            indicator.isHidden = false
+            return indicator
         }()
-        label.padding(top: 10, left: 10, bottom: 10, right: 10)
-        
-        return label
+        return indicator
     }
 }
 
+private func makeToast(message: String) -> SunLabel {
+    let label: SunLabel = {
+        let label = SunLabel()
+        
+        label.backgroundColor = .black.withAlphaComponent(0.5)
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 20.0)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0
+        label.textAlignment = .center
+        label.alpha = 1.0
+        label.text = message
+        label.layer.cornerRadius = label.font.pointSize / 2
+        label.clipsToBounds = true
+        return label
+    }()
+    label.padding(top: 5, left: 10, bottom: 5, right: 10)
+    
+    return label
+    
+}
+
+
+//MARK: - SunLabel
 public final class SunLabel: UILabel {
-    private var padding: UIEdgeInsets = UIEdgeInsets()//.init(top: 0, left: 0, bottom: 0, right: 0)
+    private var padding: UIEdgeInsets = UIEdgeInsets()
     
     public func padding(top: CGFloat,left: CGFloat,bottom: CGFloat,right: CGFloat) {
         padding.top = top
@@ -128,7 +283,12 @@ public final class SunLabel: UILabel {
         var contentSize = super.intrinsicContentSize
         contentSize.height += padding.top + padding.bottom
         contentSize.width += padding.left + padding.right
-
+        
         return contentSize
     }
+}
+
+//MARK: - SunIndicator
+public final class SunIndicator: UIActivityIndicatorView {
+    
 }
